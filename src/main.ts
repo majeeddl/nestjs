0ee2ea5527989bd5708fdcc34c9c852211ecbdc4
majeed <majeed.dl@gmail.com>
2022-07-18 00:11:42 +0300

    add open api to nest js project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+      .setTitle('Nest js tutorials')
+      .setDescription('The Nest js API description')
+      .setVersion('1.0.0')
+      .addTag('nest js')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
