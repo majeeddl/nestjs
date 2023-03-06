8564af4b77302ad673ceda5222aa2caf889071c2
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2023-03-06 16:51:22 +0300

    implement first e2e test for microservices

--- microservices/service-consumer/test/app.e2e-spec.ts
+++ microservices/service-consumer/test/app.e2e-spec.ts
@@ -2,23 +2,57 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { ClientsModule } from '@nestjs/microservices/module';
+import { ClientProxy, Transport } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let client: ClientProxy;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        AppModule,
+        ClientsModule.register([
+          {
+            name: 'SERVICE_PROVIDER',
+            transport: Transport.TCP,
+          },
+        ]),
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    app.connectMicroservice({
+      transport: Transport.TCP,
+    });
+
+    // await app.startAllMicroservices();
     await app.init();
+
+    client = app.get('SERVICE_PROVIDER');
+    await client.connect();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+    client.close();
+  });
+  // it('/ (GET)', () => {
+  //   return request(app.getHttpServer())
+  //     .get('/')
+  //     .expect(200)
+  //     .expect('Hello World!');
+  // });
+
+  it('first microservice test', async () => {
+    const pattern = { cmd: 'getHello' };
+    const payload = [1, 2, 3];
+    const command = client.send<string>(pattern, payload);
+    const data: string = await lastValueFrom(command);
+
+    expect(data).toBe('hello from service-provider');
   });
 });
