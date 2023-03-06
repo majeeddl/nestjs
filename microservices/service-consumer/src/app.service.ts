dd1a0d05e1bc36cafd36e70ef16405ba1ed83d3c
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2023-03-06 16:35:51 +0300

    develope first microservice

--- microservices/service-consumer/src/app.service.ts
+++ microservices/service-consumer/src/app.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices/client';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  /**
+   *
+   */
+  constructor(@Inject('SERVICE_PROVIDER') private client: ClientProxy) {}
+
+  async getHello() {
+    const pattern = { cmd: 'getHello' };
+    const payload = [1, 2, 3];
+    const command = this.client.send<string>(pattern, payload);
+    const data:string = await lastValueFrom(command);
+    console.log(data);
+    return data;
   }
 }
